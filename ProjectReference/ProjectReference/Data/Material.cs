af581d0cae6d17b0a43d289fab2264dd81e96247
Dusan Park <qkrentks456@gmail.com>
2026-07-13 19:47:19 +0900

    Chapter3 Foldering, Make Zip
    학습 제공용 chapter 분할 및 zip파일 생성

--- ProjectReference/ProjectReference/Data/Material.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Material
-{
-    public virtual bool Scatter(out Ray r, out HitRecord rec, ref Color attenuation, ref Ray scattered)
-    {
-        r = new Ray();
-        rec = new HitRecord();
-        return false;
-    }
-}
